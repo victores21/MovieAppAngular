c88804d34a5b8ef85f721e5295c36b2f66c9b3f2
Victor Escalona <victorescalonag@gmail.com>
2021-02-12 08:22:27 -0500

    Added router-outlet tag to app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, MovieDetailComponent],
   imports: [BrowserModule, AppRoutingModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Components
 import { HomeComponent } from './components/home/home.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 //Routing Variable
 
-const APP_ROUTES: Routes = [{ path: 'home', component: HomeComponent }];
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'movie/:id', component: MovieDetailComponent },
+];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
