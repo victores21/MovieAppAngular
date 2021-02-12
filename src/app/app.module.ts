42c1cb76146efc349f1232b315de883e1c4c184d
Victor Escalona <victorescalonag@gmail.com>
2021-02-12 08:30:04 -0500

    Created components for navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MovieDetailComponent],
+  declarations: [AppComponent, HomeComponent, MovieDetailComponent, NavbarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent],
