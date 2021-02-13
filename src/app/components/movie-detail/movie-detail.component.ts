174c5b7d9aec1e9370cf738b0b75c5ebf218447d
Victor Escalona <victorescalonag@gmail.com>
2021-02-12 23:32:11 -0500

    Added API SERVICE & added a function to get the movie ID from the url

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  styleUrls: ['./movie-detail.component.scss'],
 })
 export class MovieDetailComponent implements OnInit {
+  movieId: string = '';
+  movieDetails: any[] = [];
 
-  constructor() { }
+  constructor(public api: ApiService, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params.subscribe((data) => {
+      this.movieId = data.id;
+    });
 
+    this.api
+      .getListOfMovies(
+        'https://api.themoviedb.org/3/movie/' +
+          this.movieId +
+          '?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US'
+      )
+      .subscribe((data: any) => {
+        this.movieDetails = data.results;
+      });
+  }
 }
