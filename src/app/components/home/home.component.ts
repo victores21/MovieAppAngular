1c60839df580fc2b22ae49227c8d610c070be687
Victor Escalona <victorescalonag@gmail.com>
2021-02-14 01:14:47 -0500

    Added a loading sponner while the data is beeing fetched

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,19 +11,42 @@ export class HomeComponent implements OnInit {
   popularMovies: any[] = [];
   topRatedMovies: any[] = [];
   upcomingMovies: any[] = [];
-  constructor(public api: ApiService, private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.queryParams.subscribe((data) => {
-      console.log(data);
-    });
-  }
+  isPopularMoviesloading: boolean = true;
+  isTopRatedMoviesloading: boolean = true;
+  isUpcomingMoviesloading: boolean = true;
 
+  constructor(
+    public api: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  isEverythingLoaded() {
+    if (
+      this.isPopularMoviesloading ||
+      this.isTopRatedMoviesloading ||
+      this.isUpcomingMoviesloading
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   ngOnInit(): void {
+    // this.activatedRoute.queryParams.subscribe((data) => {
+    //   console.log(data);
+    // });
+    this.spinner.show();
     this.api
       .getListOfMovies(
         'https://api.themoviedb.org/3/movie/popular?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US&page=1'
       )
       .subscribe((data: any) => {
         this.popularMovies = data.results;
+        this.isPopularMoviesloading = false;
+        if (this.isEverythingLoaded()) {
+          this.spinner.hide();
+        }
       });
 
     this.api
@@ -31,6 +55,10 @@ export class HomeComponent implements OnInit {
       )
       .subscribe((data: any) => {
         this.topRatedMovies = data.results;
+        this.isTopRatedMoviesloading = false;
+        if (this.isEverythingLoaded()) {
+          this.spinner.hide();
+        }
       });
 
     this.api
@@ -39,6 +67,10 @@ export class HomeComponent implements OnInit {
       )
       .subscribe((data: any) => {
         this.upcomingMovies = data.results;
+        this.isUpcomingMoviesloading = false;
+        if (this.isEverythingLoaded()) {
+          this.spinner.hide();
+        }
       });
   }
 }
