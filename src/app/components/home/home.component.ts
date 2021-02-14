c9c2c0705758ea8b4161079597ba4dd3c1362545
Victor Escalona <victorescalonag@gmail.com>
2021-02-14 01:39:20 -0500

    Now all the fetches in this components are using enviroment variables

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -21,56 +22,51 @@ export class HomeComponent implements OnInit {
     private spinner: NgxSpinnerService
   ) {}
 
-  isEverythingLoaded() {
-    if (
-      this.isPopularMoviesloading ||
-      this.isTopRatedMoviesloading ||
-      this.isUpcomingMoviesloading
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  // isEverythingLoaded() {
+  //   if (
+  //     this.isPopularMoviesloading ||
+  //     this.isTopRatedMoviesloading ||
+  //     this.isUpcomingMoviesloading
+  //   ) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+
   ngOnInit(): void {
-    // this.activatedRoute.queryParams.subscribe((data) => {
-    //   console.log(data);
-    // });
-    this.spinner.show();
+    // this.spinner.show();
     this.api
       .getListOfMovies(
-        'https://api.themoviedb.org/3/movie/popular?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US&page=1'
+        'https://api.themoviedb.org/3/movie/popular?api_key=' +
+          environment.API_KEY +
+          '&language=en-US&page=1'
       )
       .subscribe((data: any) => {
         this.popularMovies = data.results;
         this.isPopularMoviesloading = false;
-        if (this.isEverythingLoaded()) {
-          this.spinner.hide();
-        }
       });
 
     this.api
       .getListOfMovies(
-        'https://api.themoviedb.org/3/movie/top_rated?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US&page=1'
+        'https://api.themoviedb.org/3/movie/top_rated?api_key=' +
+          environment.API_KEY +
+          '&language=en-US&page=1'
       )
       .subscribe((data: any) => {
         this.topRatedMovies = data.results;
         this.isTopRatedMoviesloading = false;
-        if (this.isEverythingLoaded()) {
-          this.spinner.hide();
-        }
       });
 
     this.api
       .getListOfMovies(
-        'https://api.themoviedb.org/3/movie/upcoming?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US&page=1'
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=' +
+          environment.API_KEY +
+          '&language=en-US&page=1'
       )
       .subscribe((data: any) => {
         this.upcomingMovies = data.results;
         this.isUpcomingMoviesloading = false;
-        if (this.isEverythingLoaded()) {
-          this.spinner.hide();
-        }
       });
   }
 }
