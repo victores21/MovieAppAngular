8c75f48b9bd77a9d3ca6fcea1ad10757feeb1479
Victor Escalona <victorescalonag@gmail.com>
2021-02-13 22:02:55 -0500

    Fixed movies/cast poster width

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
 @Component({
@@ -8,24 +8,52 @@ import { ApiService } from '../../services/api.service';
   styleUrls: ['./movie-detail.component.scss'],
 })
 export class MovieDetailComponent implements OnInit {
-  movieId: string = '';
-  movieDetails: any[] = [];
-
-  constructor(public api: ApiService, private activatedRoute: ActivatedRoute) {}
+  movieId: any;
+  movieDetail: any = {};
+  similarMovies: any[] = [];
+  castList: any[] = [];
+  constructor(
+    public api: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((data) => {
-      this.movieId = data.id;
-    });
+    console.log('CARANDO');
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      this.movieId = params.get('id');
 
-    this.api
-      .getListOfMovies(
-        'https://api.themoviedb.org/3/movie/' +
-          this.movieId +
-          '?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US'
-      )
-      .subscribe((data: any) => {
-        this.movieDetails = data.results;
-      });
+      this.api
+        .getListOfMovies(
+          'https://api.themoviedb.org/3/movie/' +
+            this.movieId +
+            '?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US'
+        )
+        .subscribe((data: any) => {
+          this.movieDetail = data;
+          console.log('movie detail', this.movieDetail);
+        });
+
+      this.api
+        .getListOfMovies(
+          'https://api.themoviedb.org/3/movie/' +
+            this.movieId +
+            '/similar?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US&page=1'
+        )
+        .subscribe((data: any) => {
+          this.similarMovies = data.results;
+        });
+
+      this.api
+        .getListOfMovies(
+          ' https://api.themoviedb.org/3/movie/' +
+            this.movieId +
+            '/credits?api_key=27337898bc0c9b2c97bc056520acb400&language=en-US'
+        )
+        .subscribe((data: any) => {
+          this.castList = data.cast;
+          console.log('cast list', this.castList);
+        });
+    });
   }
 }
