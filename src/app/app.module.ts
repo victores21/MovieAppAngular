21d486eb2468f128353acd9eada6452288762069
Victor Escalona <victorescalonag@gmail.com>
2021-02-13 22:01:28 -0500

    Imported NgbModule-FormsModule and the new search list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { HomeComponent } from './components/home/home.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { SearchListComponent } from './components/search-list/search-list.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,8 +22,16 @@ import { FooterComponent } from './components/footer/footer.component';
     MovieDetailComponent,
     NavbarComponent,
     FooterComponent,
+    SearchListComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, APP_ROUTING, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
