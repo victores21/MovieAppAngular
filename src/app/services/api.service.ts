e680241f22cf54a700d1e77f1654c1753c367c4d
Victor Escalona <victorescalonag@gmail.com>
2021-02-14 22:58:20 -0500

    Added interfaces to the services functions

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { ListOfMovies } from '../shared/interfaces/listOfMovies.interface';
+import { movieDetail } from '../shared/interfaces/movieDetail.interface';
 @Injectable({
   providedIn: 'root',
 })
@@ -8,6 +9,10 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getListOfMovies(url: string) {
-    return this.http.get(url);
+    return this.http.get<ListOfMovies>(url);
+  }
+
+  getMovieDetail(url: string) {
+    return this.http.get<movieDetail>(url);
   }
 }
