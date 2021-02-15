629e64756f63f3f52145ab51bea9a5d248c9d6db
Victor Escalona <victorescalonag@gmail.com>
2021-02-14 22:57:04 -0500

    Added a new folder to include the app interfaces

---
+++ src/app/shared/interfaces/listOfMovies.interface.ts
@@ -0,0 +1,6 @@
+export interface ListOfMovies {
+  page: number;
+  results: string[];
+  total_pages: number;
+  total_results: number;
+}
---
+++ src/app/shared/interfaces/movieDetail.interface.ts
@@ -0,0 +1,27 @@
+export interface movieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: any;
+  budget: number;
+  genres: string[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: string[];
+  production_countries: string[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: string[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
