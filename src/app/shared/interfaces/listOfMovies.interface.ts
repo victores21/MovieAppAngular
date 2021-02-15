fecf2b8d5c26ac6d3e2b6ec5ebe384580a6d2c0b
Victor Escalona <victorescalonag@gmail.com>
2021-02-14 23:18:43 -0500

    Added a new interface for full query data and for list of movies data

--- src/app/shared/interfaces/listOfMovies.interface.ts
+++ src/app/shared/interfaces/listOfMovies.interface.ts
@@ -1,6 +1,23 @@
-export interface ListOfMovies {
+export interface ListOfMoviesQuery {
   page: number;
   results: string[];
   total_pages: number;
   total_results: number;
+}
+
+export interface ListOfMovies {
+  adult: number;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
